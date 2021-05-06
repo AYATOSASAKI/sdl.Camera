ebfdb14cec4d442b9ed984d0f37facb332de3a58
AYATOSASAKI <65755265+AYATOSASAKI@users.noreply.github.com>
2021-05-06 21:29:30 +0900

    homework3  カメラを起動してメインに表示する機能を実装

--- app/src/main/java/jp/ac/titech/itpro/sdl/camera/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/camera/MainActivity.java
@@ -9,15 +9,15 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
-
+import android.provider.MediaStore;
 import androidx.appcompat.app.AppCompatActivity;
-
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private final static int REQ_PHOTO = 1234;
     private Bitmap photoImage = null;
+    //ImageView imageView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
         Button photoButton = findViewById(R.id.photo_button);
         photoButton.setOnClickListener(v -> {
-            Intent intent = new Intent();
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             // TODO: You should setup appropriate parameters for the intent
 
             PackageManager manager = getPackageManager();
@@ -54,6 +54,9 @@ public class MainActivity extends AppCompatActivity {
         if (reqCode == REQ_PHOTO) {
             if (resCode == RESULT_OK) {
                 // TODO: You should implement the code that retrieve a bitmap image
+                ImageView photoView = findViewById(R.id.photo_view);
+                Bitmap capturedImage = (Bitmap) data.getExtras().get("data");
+                photoView.setImageBitmap(capturedImage);
             }
         }
     }
